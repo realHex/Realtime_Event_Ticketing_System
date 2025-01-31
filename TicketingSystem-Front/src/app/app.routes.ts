82be56a9d4967e557360d3ebcd9029f96e16ec22
realHex <sajitha.20220784@iit.ac.lk>
2025-01-31 19:05:32 +0530

    Added new pages

--- TicketingSystem-Front/src/app/app.routes.ts
+++ TicketingSystem-Front/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SimulationComponent } from './pages/simulation/simulation.component';
+import { VendorComponent } from './pages/vendor/vendor.component';
+import { CustomerComponent } from './pages/customer/customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'simulation', pathMatch: 'full' },
+  { path: 'simulation', component: SimulationComponent },
+  { path: 'vendor', component: VendorComponent },
+  { path: 'customer', component: CustomerComponent }
+];
