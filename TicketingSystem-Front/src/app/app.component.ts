318669eb73c5cb4643dfb69f5de9fa0e4c82d964
realHex <sajitha.20220784@iit.ac.lk>
2025-01-31 19:20:04 +0530

    Moving simulation to the new page

--- TicketingSystem-Front/src/app/app.component.ts
+++ TicketingSystem-Front/src/app/app.component.ts
@@ -1,26 +1,13 @@
 import { Component } from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {ControlsTopbarComponent} from './components/controls-topbar/controls-topbar.component';
-import {AddUserComponent} from './components/add-user/add-user.component';
-import {ConfigurationFormComponent} from './components/configuration-form/configuration-form.component';
-import {TicketCountsComponent} from './components/ticket-counts/ticket-counts.component';
-import {ProgressBarComponent} from './components/progress-bar/progress-bar';
-import {ChartComponent} from './components/chart/chart.component';
-import {LogsComponent} from './components/logs/logs.component';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,
-    ControlsTopbarComponent,
-    AddUserComponent,
-    ConfigurationFormComponent,
-    TicketCountsComponent,
-    ProgressBarComponent,
-    ChartComponent,
-    LogsComponent],
-  templateUrl: './app.component.html',
   standalone: true,
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'TicketingSystem-Front';
--- TicketingSystem-Front/src/app/pages/simulation/simulation.component.ts
+++ TicketingSystem-Front/src/app/pages/simulation/simulation.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { ControlsTopbarComponent } from '../../components/controls-topbar/controls-topbar.component';
+import { AddUserComponent } from '../../components/add-user/add-user.component';
+import { ConfigurationFormComponent } from '../../components/configuration-form/configuration-form.component';
+import { TicketCountsComponent } from '../../components/ticket-counts/ticket-counts.component';
+import { ProgressBarComponent } from '../../components/progress-bar/progress-bar';
+import { ChartComponent } from '../../components/chart/chart.component';
+import { LogsComponent } from '../../components/logs/logs.component';
 
 @Component({
   selector: 'app-simulation',
   standalone: true,
-  imports: [],
+  imports: [
+    ControlsTopbarComponent,
+    AddUserComponent,
+    ConfigurationFormComponent,
+    TicketCountsComponent,
+    ProgressBarComponent,
+    ChartComponent,
+    LogsComponent
+  ],
   templateUrl: './simulation.component.html',
-  styleUrl: './simulation.component.css'
+  styleUrls: ['./simulation.component.css']
 })
 export class SimulationComponent {
 
